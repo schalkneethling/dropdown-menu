30ab572a500ef83c317a5140738783c2dec4dd44
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-09-30 02:21:09 +0200

    working v1

--- js/main.js
+++ js/main.js
@@ -3,14 +3,34 @@
 
   const mainNav = document.getElementById("main-nav");
 
+  function closeAll() {
+    const dropdownList = document.querySelectorAll(".dropdown-list");
+    dropdownList.forEach((dropdown) => {
+      dropdown.classList.remove("show");
+      dropdown.previousElementSibling.setAttribute("aria-expanded", "false");
+    });
+  }
+
   if (mainNav) {
     mainNav.addEventListener("click", (event) => {
       const isDropdownTrigger =
         event.target.classList.contains("dropdown-trigger");
+      const isExpanded = event.target.getAttribute("aria-expanded") === "true";
       if (isDropdownTrigger) {
+        // close any currently open dropdowns,
+        // unless the clicked dropdown is already open
+        if (!isExpanded) {
+          closeAll();
+        }
+
         const dropdownTrigger = event.target;
         const dropdown = document.getElementById(
           dropdownTrigger.getAttribute("aria-controls")
+        );
+
+        dropdownTrigger.setAttribute(
+          "aria-expanded",
+          isExpanded ? false : true
         );
         dropdown.classList.toggle("show");
       }
